fb646f4dcde444e1bae5a8ab20346942c9467daa
Anton2319 <anton.rudkovskij1993@gmail.com>
2019-01-05 18:14:47 +0300

    Edit test

--- test/com/company/TriangleTest.java
+++ test/com/company/TriangleTest.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 public class TriangleTest {
     @Test
     public void test() {
-        Assert.assertEquals(1, Triangle.buildTriangle(2.0, 2.0, 2.0));
+        Assert.assertEquals(2, Triangle.buildTriangle(2.0, 2.0, 2.0));
         Assert.assertEquals(0, Triangle.buildTriangle(500.0, 2.0, 2.0));
-        Assert.assertEquals(1,  Triangle.buildTriangle(5.0, 3.0, 2.0));
-        Assert.assertEquals(1,  Triangle.buildTriangle(2.0, 5.0, 3.0));
-        Assert.assertEquals(1,  Triangle.buildTriangle(2.0, 3.0, 5.0));
+        Assert.assertEquals(1,  Triangle.buildTriangle(4.0, 3.0, 2.0));
+        Assert.assertEquals(1,  Triangle.buildTriangle(2.0, 4.0, 3.0));
+        Assert.assertEquals(1,  Triangle.buildTriangle(2.0, 3.0, 4.0));
         Assert.assertEquals(0, Triangle.buildTriangle(2.0, 3.0, 5.000000001));
         Assert.assertEquals(0, Triangle.buildTriangle(0.0, 0.0, 0.0));
         Assert.assertEquals(0, Triangle.buildTriangle(-10.0, -10.0, -10.0));
