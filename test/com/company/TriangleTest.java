d783edb5ad9cb4b278fe51690837703a302f45e6
Anton2319 <anton.rudkovskij1993@gmail.com>
2019-01-05 17:53:47 +0300

    Added some tests

--- test/com/company/TriangleTest.java
+++ test/com/company/TriangleTest.java
@@ -1,7 +1,19 @@
 package com.company;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-class TriangleTest {
-
+public class TriangleTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(Triangle.buildTriangle(2.0, 2.0, 2.0));
+        Assert.assertFalse(Triangle.buildTriangle(500.0, 2.0, 2.0));
+        Assert.assertTrue(Triangle.buildTriangle(5.0, 3.0, 2.0));
+        Assert.assertTrue(Triangle.buildTriangle(2.0, 5.0, 3.0));
+        Assert.assertTrue(Triangle.buildTriangle(2.0, 3.0, 5.0));
+        Assert.assertFalse(Triangle.buildTriangle(2.0, 3.0, 5.000000001));
+        Assert.assertFalse(Triangle.buildTriangle(0.0, 0.0, 0.0));
+        Assert.assertFalse(Triangle.buildTriangle(-10.0, -10.0, -10.0));
+        Assert.assertFalse(Triangle.buildTriangle(-10.0, -10.0, -10.0));
+    }
 }
